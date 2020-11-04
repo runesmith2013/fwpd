15ccd3832a3e21a0924723db9557bab686dd1e3d
Robert Barr <robertbarr.uk@gmail.com>
2020-11-04 12:01:15 +0000

    initial

---
+++ src/fwpd/core.clj
@@ -0,0 +1,39 @@
+(ns fwpd.core)
+(def filename "suspects.csv")
+
+(defn foo
+      "I don't do a whole lot."
+      [x]
+      (println x "Hello, World!"))
+
+(def vamp-keys [:name :glitter-index])
+
+(defn str->int
+      [str]
+      (Integer. str))
+
+(def conversions {:name identity :glitter-index str->int})
+
+(defn convert
+      [vamp-key value]
+      ((get conversions vamp-key) value))
+
+(defn parse
+      "Convert a CSV into rows of columns"
+      [string]
+      (map #(clojure.string/split % #",")
+           (clojure.string/split string #"\n")))    ; split on newline to create seq of strings
+
+(defn mapify
+      "Return a seq of maps like {:name \"Edward Cullen\" :glitter-index 10}"
+      [rows]
+      (map (fn [unmapped-row]
+                 (reduce (fn [row-map [vamp-key value]]
+                               (assoc row-map vamp-key (convert vamp-key value)))
+                         {}
+                         (map vector vamp-keys unmapped-row)))
+           rows))
+
+(defn glitter-filter
+      [minimum-glitter records]
+      (filter #(>= (:glitter-index %) minimum-glitter) records))
